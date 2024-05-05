e5d94b1e41b94929b6b27f849acc06d1411504de
velezvanessa <velezvanessa193@gmail.com>
2024-05-05 09:00:54 -0500

    muestra pokemon aleatorio y todos lospokemons

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, render_template, url_for
+import random
+import json
+
+app = Flask(__name__)
+
+
+
+
+def get_container_id():
+    try:
+        # Leer la primera línea del archivo /proc/self/cgroup
+        with open('/proc/self/cgroup', 'r') as file:
+            first_line = file.readline()
+        # Extraer el ID del contenedor de la línea
+        # El formato generalmente es algo como "12:devices:/docker/93d...54b"
+        return first_line.split('/')[-1].strip()
+    except Exception as e:
+        return f"Error al obtener el ID del contenedor: {e}"
+    
+# Carga los datos de los pokeneas desde un archivo JSON
+def cargar_pokeneas():
+    with open('data/pokeneas.json', 'r') as archivo:
+        pokeneas = json.load(archivo)
+    print(pokeneas)  # Esto imprimirá el contenido de pokeneas en la consola
+    return pokeneas
+
+pokeneas = cargar_pokeneas()
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+    
+    
+
+#pokenea aleotario
+@app.route('/api/pokenea')
+def api_pokenea():
+    pokenea = random.choice(pokeneas)
+    pokenea['id_contenedor'] = get_container_id()  # Asumiendo que tienes una función para obtener esto
+    return render_template('filosofico.html',pokeneas=pokenea)
+
+
+#Acá se muestra el pokenea
+@app.route('/pokeneas')
+def show_pokeneas():
+    return render_template('pokenea.html', pokeneas=pokeneas)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
