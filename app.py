9d6f28a37291889c5a3217ac4e52f4afd87b5f0b
luisapolanco <lmpolanco1@eafit.edu.co>
2024-05-13 14:44:50 -0500

    container_id added

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, jsonify, render_template, url_for
 import random
 import json
+import os 
 
 app = Flask(__name__)
 
 
-
-
-def get_container_id():
-    try:
-        # Leer la primera línea del archivo /proc/self/cgroup
-        with open('/proc/self/cgroup', 'r') as file:
-            first_line = file.readline()
-        # Extraer el ID del contenedor de la línea
-        # El formato generalmente es algo como "12:devices:/docker/93d...54b"
-        return first_line.split('/')[-1].strip()
-    except Exception as e:
-        return f"Error al obtener el ID del contenedor: {e}"
+# def get_container_id():
+#     try:
+#         # Leer la primera línea del archivo /proc/self/cgroup
+#         with open('/proc/self/cgroup', 'r') as file:
+#             first_line = file.readline()
+#         # Extraer el ID del contenedor de la línea
+#         # El formato generalmente es algo como "12:devices:/docker/93d...54b"
+#         return first_line.split('/')[-1].strip()
+#     except Exception as e:
+#         return f"Error al obtener el ID del contenedor: {e}"
     
 # Carga los datos de los pokeneas desde un archivo JSON
 def cargar_pokeneas():
@@ -29,26 +28,25 @@ pokeneas = cargar_pokeneas()
 
 @app.route('/')
 def index():
-    return render_template('base.html')
-
-    
+    return render_template('base.html')    
     
 
 #pokenea aleotario
 @app.route('/api/pokenea')
 def api_pokenea():
     pokenea = random.choice(pokeneas)
-    pokenea['id_contenedor'] = get_container_id()  # Asumiendo que tienes una función para obtener esto
+    #pokenea['id_contenedor'] = os.uname()[1]
     return render_template('filosofico.html',pokeneas=pokenea)
 
 
 #Acá se muestra el pokenea
 @app.route('/pokeneas')
 def show_pokeneas():
+    #pokeneas['id_contenedor'] =  os.uname()[1]
     return render_template('pokenea.html', pokeneas=pokeneas)
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
